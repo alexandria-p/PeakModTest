ba9089924efbb97b7b9a5af2c20168c5f1debecf
Hamunii <samuel.suni04@gmail.com>
2025-06-20 14:01:25 +0300

    minor cleanup

--- content/LCBepinExTemplateSimple/src/BepInExModTemplate/BepinexPlugin.cs
+++ content/LCBepinExTemplateSimple/src/BepInExModTemplate/BepinexPlugin.cs
@@ -1,46 +1,32 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
-using HarmonyLib;
 
 namespace BepInExModTemplate;
 
-/*
-  Here are some basic resources on code style and naming conventions to help
-  you in your first CSharp plugin!
+// Here are some basic resources on code style and naming conventions to help
+// you in your first CSharp plugin!
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
+// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
 
-  https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-  https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-  https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
-*/
-
-[BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
 {
-  internal static ManualLogSource Log = null!;
+    internal static ManualLogSource Log { get; private set; } = null!;
 
-  private void Awake()
-  {
-    /*
-      BepinEx makes you a ManualLogSource for free called "Logger"
-      and I created a static value above to hold on to it so other
-      parts of your plugin's code can find it by using Plugin.Log
+    private void Awake()
+    {
+        // BepInEx gives us a logger which we can use to log information.
+        // See https://lethal.wiki/dev/fundamentals/logging
+        Log = Logger;
 
-      We assign it here
-    */
-    Log = Logger;
+        // BepInEx also gives us a config file for easy configuration.
+        // See https://lethal.wiki/dev/intermediate/custom-configs
 
-    /*
-        BepinEx also gives you a config file based on your plugin's GUID
-        in the BepinEx/config folder. You can define what settings should
-        appear in it with the Config.Bind suite of methods
+        // We can apply our hooks here.
+        // See https://lethal.wiki/dev/fundamentals/patching-code
 
-        If the file doesn't exist it will be created with the defaults
-        you specify, neato!
-    */
-
-    // Log our awake here so we can see it in LogOutput.txt file
-    Log.LogInfo($"Plugin {LCMPluginInfo.PLUGIN_NAME} version {LCMPluginInfo.PLUGIN_VERSION} is loaded!");
-
-  }
-
+        // Log our awake here so we can see it in LogOutput.log file
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
 }
