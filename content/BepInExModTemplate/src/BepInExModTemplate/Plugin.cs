e93a2deb2aa88947f682b363568396413542a2f5
Hamunii <samuel.suni04@gmail.com>
2025-06-21 20:09:48 +0300

    add link to AutoPlugin repository

--- content/BepInExModTemplate/src/BepInExModTemplate/Plugin.cs
+++ content/BepInExModTemplate/src/BepInExModTemplate/Plugin.cs
@@ -12,8 +12,7 @@ namespace BepInExModTemplate;
 
 // This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
 // NuGet package, and it will generate the BepInPlugin attribute for you!
-// You can see the usages of the 'Plugin' class in your IDE and find the
-// source generated file!
+// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
 //#endif
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin
